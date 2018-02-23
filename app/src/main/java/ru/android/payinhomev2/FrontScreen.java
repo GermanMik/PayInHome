706894f09e4603cfc5967b440b8f2d432bdd6d08
German <tiktak@yandex.ru>
2018-02-24 00:55:55 +0300

    added loginactivity

--- app/src/main/java/ru/android/payinhomev2/FrontScreen.java
+++ app/src/main/java/ru/android/payinhomev2/FrontScreen.java
@@ -6,9 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+
 public class FrontScreen extends AppCompatActivity implements View.OnClickListener {
     Button signbtn;
     Button regbtn;
+    private FirebaseAuth mAuth;
+    private static final int RC_SIGN_IN = 123;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +28,60 @@ public class FrontScreen extends AppCompatActivity implements View.OnClickListen
         setContentView(R.layout.activity_front_screen);
         signbtn = (Button) findViewById(R.id.enter_btn);
         signbtn.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+
+//        // Choose authentication providers
+//        List<AuthUI.IdpConfig> providers = Arrays.asList(
+//                new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+//                new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build(),
+//                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build());
+//
+//// Create and launch sign-in intent
+//        startActivityForResult(
+//                AuthUI.getInstance()
+//                        .createSignInIntentBuilder()
+//                        .setAvailableProviders(providers)
+//        .setLogo(R.drawable.ic_launcher_web)      // Set logo drawable
+//                .setTheme(R.style.AppTheme)      // Set theme
+//                .build(),
+//                RC_SIGN_IN);
+
     }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == RC_SIGN_IN) {
+//            IdpResponse response = IdpResponse.fromResultIntent(data);
+//
+//            if (resultCode == RESULT_OK) {
+//                // Successfully signed in
+//                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//                // ...
+//            } else {
+//                // Sign in failed, check response for error code
+//                // ...
+//            }
+//        }
+//    }
+
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        updateUI(currentUser);
+//    }
 
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-        case R.id.enter_btn:
-        Intent intent=new Intent(this,MainActivity.class);
-        startActivity(intent);
-        break;
-    }}
+            case R.id.enter_btn:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
 }
