f1bae20b5b5b2f43552723ab1543c4c8cea532bc
GermanMik <tiktak@yandex.ru>
2018-02-27 02:29:00 +0300

    Adding theme to sharedprefs

--- app/src/main/java/ru/android/payinhomev2/FrontScreen.java
+++ app/src/main/java/ru/android/payinhomev2/FrontScreen.java
@@ -1,14 +1,19 @@
 package ru.android.payinhomev2;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 
 public class FrontScreen extends AppCompatActivity implements View.OnClickListener {
+    public static final String APP_PREFERENCES_THEME = "THEME";
     Button signbtn;
     Button regbtn;
+    String pref;
+    SharedPreferences sharedSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +31,36 @@ public class FrontScreen extends AppCompatActivity implements View.OnClickListen
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-        case R.id.enter_btn:
-        Intent intent=new Intent(this,MainActivity.class);
-        startActivity(intent);
-        break;
-    }}
+            case R.id.enter_btn:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.radio_theme_light:
+                if (checked)
+                sharedSettings = getSharedPreferences(APP_PREFERENCES_THEME, 0);
+                SharedPreferences.Editor editor = sharedSettings.edit();
+                editor.putInt("theme", 0);
+                editor.commit();
+
+                break;
+            case R.id.radio_theme_dark:
+                if (checked)
+                    sharedSettings = getSharedPreferences(APP_PREFERENCES_THEME, 0);
+                editor = sharedSettings.edit();
+                //"theme" is the key sTheme is the value you're saving
+                editor.putInt("theme", 1);
+                editor.commit();
+                    break;
+        }
+    }
 }
--- app/src/main/java/ru/android/payinhomev2/MainActivity.java
+++ app/src/main/java/ru/android/payinhomev2/MainActivity.java
@@ -1,9 +1,7 @@
 package ru.android.payinhomev2;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,11 +11,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import static ru.android.payinhomev2.FrontScreen.APP_PREFERENCES_THEME;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences settings = getSharedPreferences(APP_PREFERENCES_THEME, 0);
+        int theme = settings.getInt("theme", 0);
+
+        setTheme(theme);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -81,7 +86,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_settings) {
-            // Handle the camera action
+
         } else if (id == R.id.nav_payment_adress) {
 
         } else if (id == R.id.nav_counters) {
